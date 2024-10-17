f267c6c7f4e67da66af0d9fbfd0a3b7bb3efcf98
lyonec <lyonec@kali>
2024-10-17 04:51:53 -0400

    Design food components

---
+++ react_app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FoodList from './food';
+
+function App() {
+  return (
+    <div className="App">
+      <FoodList />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_app/src/food.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const initialFoodItems = [
+  { id: "fw_hkahsfhkhsksfg", createdAt: new Date(), updatedAt: new Date() },
+  { id: "fw_hkhjksdhyuirnsf", createdAt: new Date(), updatedAt: new Date() },
+  { id: "fw_sfuirmnsisdfmri", createdAt: new Date(), updatedAt: new Date() },
+  { id: "fw_sfdlksdfjijrmffgd", createdAt: new Date(), updatedAt: new Date() },
+  { id: "fw_sfdlkjklfssjhdfs", createdAt: new Date(), updatedAt: new Date() },
+  { id: "fw_sfdlksdjfgsdhjnf", createdAt: new Date(), updatedAt: new Date() }
+];
+
+function FoodItemComponent({ foodItem }) {
+  return (
+    <div>
+      <p>FOOD</p>
+      <p className="date">{foodItem.createdAt.toLocaleString()}</p>
+      <p>ID: {foodItem.id}</p>
+      <p>Updated: {foodItem.updatedAt.toLocaleString()}</p>
+      <p>Chuck Norris doesn't believe in ravioli. He stuffs a little turtle with beef and smothers it in pig's blood.</p>
+    </div>
+  );
+}
+
+function FoodList() {
+  const [foodItems] = useState(initialFoodItems);
+  
+  return (
+    <>
+      <h4>Food, Career, Celebrity, Dev</h4>
+      <ul>
+        {foodItems.map((foodItem) => (
+          <div key={foodItem.id}>
+            <FoodItemComponent foodItem={foodItem} />
+          </div>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default FoodList;
